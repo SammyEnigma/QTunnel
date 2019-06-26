4a56c75cad871df84e882ce81404e5bd4da1e5ad
Alexander Almaleh <sashoalm@gmail.com>
2019-06-26 17:33:40 +0300

    Use command line parameters.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    new Server(&a, QHostAddress::Any, 1111, QHostAddress::LocalHost, 2222);
+    if (argc != 5) {
+        qDebug("Needs 4 parameters.");
+        qDebug("Usage: qtunnel <source address> <source port> <destination address> <destination port>");
+        qDebug("Example usage: qtunnel 0.0.0.0 81 127.0.0.1 82");
+        return 1;
+    }
+
+    new Server(&a, QHostAddress(argv[1]), QString(argv[2]).toInt(), QHostAddress(argv[3]), QString(argv[4]).toInt());
     return a.exec();
 }
